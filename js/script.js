08bb2cb170b2bd8c040b5878d724950251b61252
ESTAS-crypto <evanatharasya20@gmail.com>
2024-08-08 23:37:21 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//toggle  class active
+const navbarNav = document.querySelector(".navbar-nav");
+//menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+    navbarNav.classList.toggle("active");
+};
+//klik di luar menu menghilangkan menu
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function(e) {
+    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove("active");
+    }
+});
