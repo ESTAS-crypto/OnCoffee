5693e5367dc3c2d1924e27f113e50427d39c7d3b
ESTAS-crypto <evanatharasya20@gmail.com>
2024-08-11 11:53:30 +0700

    script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
-//toggle  class active
+//toggle  class active menu
 const navbarNav = document.querySelector(".navbar-nav");
 //menu di klik
 document.querySelector("#hamburger-menu").onclick = () => {
-    navbarNav.classList.toggle("active");
+  navbarNav.classList.toggle("active");
 };
+
+//toggle class active search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = () => {
+  searchForm.classList.toggle("active");
+};
+
 //klik di luar menu menghilangkan menu
 const hamburger = document.querySelector("#hamburger-menu");
 
-document.addEventListener("click", function(e) {
-    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-        navbarNav.classList.remove("active");
-    }
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
 });
